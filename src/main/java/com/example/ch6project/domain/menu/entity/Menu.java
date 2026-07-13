96ed8bb5437426fa297b39ff137100ecb0363d93
HAN-DABON <sinmint11@gmail.com>
2026-07-13 20:55:17 +0900

    feat: 메뉴 엔티티 작성

---
+++ src/main/java/com/example/ch6project/domain/menu/entity/Menu.java
@@ -0,0 +1,27 @@
+package com.example.ch6project.domain.menu.entity;
+
+import com.example.ch6project.common.entity.BaseTimeEntity;
+import jakarta.persistence.*;
+import lombok.AccessLevel;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+
+@Entity
+@Getter
+@NoArgsConstructor(access = AccessLevel.PROTECTED)
+@Table(name = "menus")
+public class Menu extends BaseTimeEntity {
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Long id;
+
+    @Column(nullable = false, length = 20)
+    private String name;
+
+    @Column(nullable = false)
+    private Long price;
+
+    @Enumerated(EnumType.STRING)
+    @Column(nullable = false)
+    private MenuStatus status;
+}
