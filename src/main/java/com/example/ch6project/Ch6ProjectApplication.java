f02b2b75487918c0bee71a7a8d434e6293c0d945
HAN-DABON <sinmint11@gmail.com>
2026-07-13 10:20:38 +0900

    Initial commit

---
+++ src/main/java/com/example/ch6project/Ch6ProjectApplication.java
@@ -0,0 +1,13 @@
+package com.example.ch6project;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class Ch6ProjectApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(Ch6ProjectApplication.class, args);
+    }
+
+}
---
+++ src/test/java/com/example/ch6project/Ch6ProjectApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.ch6project;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class Ch6ProjectApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
